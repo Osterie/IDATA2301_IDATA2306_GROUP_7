631d8c61196b0322361d35fd0f41460a5c825a4e
TobyJavascript <98916721+TobyJavascript@users.noreply.github.com>
2025-02-04 11:29:40 +0100

    Unpacked spring

---
+++ backend-Idata2306/Code/ntnu/ntnu/src/main/java/edu/stud/ntnu/Idata2306Application.java
@@ -0,0 +1,13 @@
+package edu.stud.ntnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Idata2306Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Idata2306Application.class, args);
+	}
+
+}
