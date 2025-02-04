c2a15957a065a899cc5ad7331174f5b74193528b
Adrian Gjøsund Bjørge <adriangb@stud.ntnu.no>
2025-02-04 11:26:20 +0100

    Changed field names for tables

--- backend-Idata2306/Resources/tables.sql
+++ backend-Idata2306/Resources/tables.sql
@@ -2,7 +2,7 @@ CREATE TABLE Price (
     flight_name VARCHAR(100) NOT NULL, -- Must match the referenced column type
     provider VARCHAR(64) NOT NULL,
     price INT NOT NULL,
-    code CHAR(3) NOT NULL,
+    price_code CHAR(3) NOT NULL,
     FOREIGN KEY (flight_name) REFERENCES Flights(flight_name)
 );
 
@@ -14,7 +14,7 @@ CREATE TABLE Flight (
 );
 
 CREATE TABLE Airport (
-    code CHAR(3) PRIMARY KEY,
+    airport_code CHAR(3) PRIMARY KEY,
     city VARCHAR(255) NOT NULL
 );
 
