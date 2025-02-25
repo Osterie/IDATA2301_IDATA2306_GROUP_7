5171c6513bc76d9c97da5b017acec140057684ac
OSTERITE <adrian@bandidos.org>
2025-02-25 14:21:37 +0100

    Configured CORS on backend

--- frontend-Idata2301/code/app/src/App.js
+++ frontend-Idata2301/code/app/src/App.js
@@ -5,30 +5,17 @@ import MainPageHero from './components/hero/mainPageHero/MainPageHero';
 import Footer from './components/footer/Footer';
 import Navbar from './components/header/Navbar';
 
-// Used to fetch data from backend
-import { useEffect, useState } from "react";
-
 function testApi (){
   
 
-      fetch('URL')
-    .then(response => response.text())  // Change from .json() to .text()
-    .then(data => console.log(data));    // Now it correctly logs "Hei, Verden!!"
+      fetch('http://localhost:8080/hello')
+    .then(response => response.text())
+    .then(data => console.log(data));
 
 
 };
 
 function App() {
-
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    fetch("http://localhost:8080/api/message")
-      .then((response) => response.text())
-      .then((data) => setMessage(data))
-      .catch((error) => console.error("Error fetching data:", error));
-  }, []);
-
   return (
     <div className="App">
 
@@ -39,7 +26,6 @@ function App() {
       <main>
 
         <button onClick={() => testApi()}>Click me</button>
-        <p>{message}</p>
         <MainPageHero/> 
         <FilterSidebar />
         <ProductCardContainer />
