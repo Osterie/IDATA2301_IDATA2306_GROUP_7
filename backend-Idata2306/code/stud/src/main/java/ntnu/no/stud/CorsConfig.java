162550851e0f23f36697959580a5a9e872910f49
Daniel <danielsel@live.no>
2025-02-25 14:51:22 +0100

    Updated sprintreports for the previous sprint

--- backend-Idata2306/code/stud/src/main/java/ntnu/no/stud/CorsConfig.java
+++ backend-Idata2306/code/stud/src/main/java/ntnu/no/stud/CorsConfig.java
@@ -1,3 +1,5 @@
+package ntnu.no.stud;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -10,7 +12,7 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
+                registry.addMapping("/api/**")
                         .allowedOrigins("http://localhost:3000") // React frontend URL
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
