d379540ba9f50688c0fb77b1ebd4b84e093d8d54
OSTERITE <adrian@bandidos.org>
2025-02-25 13:56:52 +0100

    changed cors configurer mapping

--- backend-Idata2306/code/stud/src/main/java/ntnu/no/stud/CorsConfig.java
+++ backend-Idata2306/code/stud/src/main/java/ntnu/no/stud/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**")
+                registry.addMapping("/**")
                         .allowedOrigins("http://localhost:3000") // React frontend URL
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
