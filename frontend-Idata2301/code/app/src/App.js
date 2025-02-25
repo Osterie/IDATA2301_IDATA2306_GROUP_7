b240d7c97b2df36dd7bc5de89e3a855e7f114641
TobyJavascript <98916721+TobyJavascript@users.noreply.github.com>
2025-02-25 14:09:02 +0100

    Added some test code in app for fetching a message

--- frontend-Idata2301/code/app/src/App.js
+++ frontend-Idata2301/code/app/src/App.js
@@ -5,6 +5,9 @@ import MainPageHero from './components/hero/mainPageHero/MainPageHero';
 import Footer from './components/footer/Footer';
 import Navbar from './components/header/Navbar';
 
+// Used to fetch data from backend
+import { useEffect, useState } from "react";
+
 function testApi (){
   
 
@@ -16,6 +19,16 @@ function testApi (){
 };
 
 function App() {
+
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/message")
+      .then((response) => response.text())
+      .then((data) => setMessage(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
   return (
     <div className="App">
 
@@ -26,6 +39,7 @@ function App() {
       <main>
 
         <button onClick={() => testApi()}>Click me</button>
+        <p>{message}</p>
         <MainPageHero/> 
         <FilterSidebar />
         <ProductCardContainer />
