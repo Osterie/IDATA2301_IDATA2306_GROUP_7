cb6d988fb9ad866f8e852dcbe77898284890248b
Daniel <danielsel@live.no>
2025-02-25 10:25:55 +0100

    Added unsaved files to commit

---
+++ frontend-Idata2301/code/app/src/components/searchBar/SearchBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './searchBar.css';
+
+const SearchBar = () => {
+    return (
+        <>
+            <form className="search-form">
+                <input type="text" placeholder="From" name="from" required />
+                <input type="text" placeholder="To" name="to" required />
+                <input type="date" name="departure-date" required />
+                <input type="date" name="return-date" required />
+                <button type="submit">Search Flights</button>
+            </form>
+        </>
+    );
+};
+
+export default SearchBar;
