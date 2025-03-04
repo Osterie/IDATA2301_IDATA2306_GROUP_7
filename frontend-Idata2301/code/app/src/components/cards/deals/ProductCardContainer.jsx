5d6f858375850a24610e78c1ed9c93b6750dc246
TobyJavascript <98916721+TobyJavascript@users.noreply.github.com>
2025-03-04 23:24:44 +0100

    Made some small changes

--- frontend-Idata2301/code/app/src/components/cards/deals/ProductCardContainer.jsx
+++ frontend-Idata2301/code/app/src/components/cards/deals/ProductCardContainer.jsx
@@ -6,7 +6,9 @@ const products = [
   { id: 1, title: "Product 1", description: "This is a very good product", img: "https://picsum.photos/250/150?rnd=1" },
   { id: 2, title: "Product 2", description: "This product is not that good, but we need to clear the stock anyway", img: "https://picsum.photos/250/150?rnd=2", highlighted: true },
   { id: 3, title: "Product 3", description: "This is another product", img: "https://picsum.photos/250/150?rnd=3" },
-  { id: 4, title: "Product 4", description: "You can get this for free in GitHub, but we need money", img: "https://picsum.photos/250/150?rnd=4" }
+  { id: 4, title: "Product 4", description: "You can get this for free in GitHub, but we need money", img: "https://picsum.photos/250/150?rnd=4" },
+  { id: 5, title: "Product 5", description: "This is somehow also a product", img: "https://picsum.photos/250/150?rnd=5" },
+  { id: 6, title: "Product 6", description: "We love Chuck Norris", img: "https://picsum.photos/250/150?rnd=6" }
 ];
 
 const ProductContainer = () => {
