a6c895e3d326ab19c659071586e0476119030953
TobyJavascript <98916721+TobyJavascript@users.noreply.github.com>
2025-03-11 10:39:09 +0100

    Added tables for features and accomadations

---
+++ backend-Idata2306/resources/Database/Creation/flight_accomadations.sql
@@ -0,0 +1,6 @@
+CREATE TABLE fligh_accommodations (
+    flight_id INT NOT NULL,
+    feature_id INT NOT NULL,
+    FOREIGN KEY (flight_id) REFERENCES flight(id)
+    FOREIGN KEY (feature_id) REFERENCES extra_feature(id)
+);
