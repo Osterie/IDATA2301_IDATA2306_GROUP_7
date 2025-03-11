787ca8dbdb0e39160f97242ba0059e8d28ca21db
Adrian Gjøsund Bjørge <adriangb@stud.ntnu.no>
2025-03-11 11:48:44 +0100

    fixed flight_names field

--- backend-Idata2306/Resources/Database/Creation/favorite_flights.sql
+++ backend-Idata2306/Resources/Database/Creation/favorite_flights.sql
@@ -2,6 +2,6 @@ Create Table favorite_flights (
     flight_id INT NOT NULL,
     user_id INT NOT NULL,
 
-    FOREIGN KEY (userid) REFERENCES user(id),
+    FOREIGN KEY (user_id) REFERENCES user(id),
     FOREIGN KEY (flight_id) REFERENCES flight(id)
 );
