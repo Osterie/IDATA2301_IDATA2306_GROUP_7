d0b4f48d34fccd9986bf79d5ae284ff511f55bf6
Adrian Gjøsund Bjørge <adriangb@stud.ntnu.no>
2025-03-11 13:24:49 +0100

    Added package and missing imports to entity classes

--- backend-Idata2306/code/stud/src/main/java/ntnu/no/stud/entities/ScheduledFlight.java
+++ backend-Idata2306/code/stud/src/main/java/ntnu/no/stud/entities/ScheduledFlight.java
@@ -1,10 +1,14 @@
 package ntnu.no.stud.entities;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Column;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "scheduled_flights")
