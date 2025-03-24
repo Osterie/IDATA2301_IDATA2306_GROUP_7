68755d03c9c6b96ed4f9c877466861e8befa5631
TobyJavascript <98916721+TobyJavascript@users.noreply.github.com>
2025-03-24 12:16:55 +0100

    Created dropdown for classes and amount of tickets

--- frontend-Idata2301/code/app/src/components/searchBar/SearchBar.jsx
+++ frontend-Idata2301/code/app/src/components/searchBar/SearchBar.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './searchBar.css';
 
 const SearchBar = () => {
+    const [showDropdown, setShowDropdown] = useState(false);
+
     return (
         <>
             <form className="search-form">
@@ -9,6 +11,23 @@ const SearchBar = () => {
                 <input type="text" placeholder="To" name="to" required />
                 <input type="date" name="departure-date" required />
                 <input type="date" name="return-date" required />
+                
+                <button type="button" className="dropdown-button" onClick={() => setShowDropdown(!showDropdown)}>
+                    Select Passengers
+                </button>
+                
+                {showDropdown && (
+                    <div className="dropdown-window">
+                        <select name="Classes" required>
+                            <option value="0">First class</option>
+                            <option value="1">Business class</option>
+                            <option value="2">Economy class</option>
+                        </select>
+                        
+                        <input className="adult-input" type="number" />
+                    </div>
+                )}
+                
                 <button type="submit">Search For Flights</button>
             </form>
         </>
