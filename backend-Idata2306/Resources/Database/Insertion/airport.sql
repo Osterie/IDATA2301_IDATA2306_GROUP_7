a8584ad9a6095b159a207414561ef89d2ac0d6e6
Adrian Gjøsund Bjørge <adriangb@stud.ntnu.no>
2025-03-25 11:23:13 +0100

    Created insert statements for airport and class table, prepared for more insert statements

---
+++ backend-Idata2306/Resources/Database/Insertion/airport.sql
@@ -0,0 +1,19 @@
+INSERT INTO flight_application.airport (airport_code, city)
+VALUES
+  ('JFK', 'New York'),
+  ('LAX', 'Los Angeles'),
+  ('ORD', 'Chicago'),
+  ('AES', 'Ålesund'),
+  ('AMS', 'Amsterdam'),
+  ('ZRH', 'Zurich'),
+  ('LHR', 'London Heathrow'),
+  ('FCO', 'Rome'),
+  ('CDG', 'Paris'),
+  ('DFW', 'Dallas'),
+  ('FRA', 'Frankfurt'),
+  ('HND', 'Tokyo'),
+  ('DXB', 'Dubai'),
+  ('DOH', 'Doha'),
+  ('SYD', 'Sydney'),
+  ('SIN', 'Singapore');
+  
