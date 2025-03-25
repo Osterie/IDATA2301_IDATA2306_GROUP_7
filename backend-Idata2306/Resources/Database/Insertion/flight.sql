773e9a7de6027b9fbbd930d746f7063a955cff97
Adrian Gjøsund Bjørge <adriangb@stud.ntnu.no>
2025-03-25 11:41:42 +0100

    Insertion of data into extra_features and flight table

---
+++ backend-Idata2306/Resources/Database/Insertion/flight.sql
@@ -0,0 +1,13 @@
+INSERT INTO flight_application.flight (company, name)
+	VALUES
+		('Delta Air Lines', 'Delta Flight 425'),
+        ('Norwegian Air Shuttle', 'Norwegian Flight 708'),
+        ('KLM Royal Dutch Airlines', 'KLM Flight 605'),
+        ('Swiss International Air Lines', 'Swiss Flight 110'),
+        ('Alitalia', 'Alitalia Flight 560'),
+        ('American Airlines', 'AA Flight 220'),
+        ('Lufthansa', 'LH Flight 445'),
+        ('Air France', 'AF Flight 123'),
+        ('Emirates', 'EK Flight 204'),
+        ('Qatar Airways', 'QR Flight 905'),
+        ('Singapore Airlines', 'SQ Flight 26');
