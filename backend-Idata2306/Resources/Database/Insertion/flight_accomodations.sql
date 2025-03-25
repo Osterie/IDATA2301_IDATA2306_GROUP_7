bf727a8feb105b918413b3de661a03d114ceacae
Adrian Gjøsund Bjørge <adriangb@stud.ntnu.no>
2025-03-25 11:52:01 +0100

    Filled table for flight accomodations and flight classes

---
+++ backend-Idata2306/Resources/Database/Insertion/flight_accomodations.sql
@@ -0,0 +1,13 @@
+INSERT INTO flight_application.flight_accommodation (feature_id, flight_id) 
+VALUES
+    (1, 18), (2, 18), (3, 18),   -- Delta Flight 425
+    (4, 19), (5, 19), (6, 19),   -- Norwegian Flight 708
+    (7, 20), (8, 20), (9, 20),   -- KLM Flight 605
+    (10, 21), (11, 21), (12, 21), -- Swiss Flight 110
+    (13, 22), (14, 22), (15, 22), -- Alitalia Flight 560
+    (16, 23), (17, 23), (18, 23), -- AA Flight 220
+    (19, 24), (20, 24), (21, 24), -- Lufthansa Flight 445
+    (22, 25), (23, 25), (24, 25), -- Air France Flight 123
+    (25, 26), (26, 26), (27, 26), -- Emirates Flight 204
+    (28, 27), (29, 27), (30, 27), -- Qatar Airways Flight 905
+    (31, 28), (32, 28), (1, 28);  -- Singapore Airlines Flight 26
