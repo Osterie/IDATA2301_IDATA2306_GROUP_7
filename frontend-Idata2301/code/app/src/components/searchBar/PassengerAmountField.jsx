66f0aad3df36caa865e85b0cef88bb9a04e88eea
TobyJavascript <98916721+TobyJavascript@users.noreply.github.com>
2025-03-25 11:59:24 +0100

    Made a custom input field

--- frontend-Idata2301/code/app/src/components/searchBar/PassengerAmountField.jsx
+++ frontend-Idata2301/code/app/src/components/searchBar/PassengerAmountField.jsx
@@ -1,23 +1,41 @@
-import './passengerAmountField.css';
+import { useState } from "react";
+import "./passengerAmountField.css";
 
 const PassengerAmountField = () => {
+    const [amount, setAmount] = useState(1400);
+
+    const addValue = () => {
+        setAmount((prev) => prev + 1);
+    };
+
+    const subtractValue = () => {
+        setAmount((prev) => prev - 1);
+    };
 
     return (
-        <>
-            <div className="dropdown-window">
-                <select name="Classes" required>
-                    <option value="0">Economy class</option>
-                    <option value="1">Business class</option>
-                    <option value="2">First class</option>
-                </select>
+        <div className="dropdown-window">
+            <select name="Classes" required>
+                <option value="0">Economy class</option>
+                <option value="1">Business class</option>
+                <option value="2">First class</option>
+            </select>
 
-                <label for="adults">Passengers 18+:</label>   
-                <input id="adults" className="adult-input" type="number" placeholder="1" />
+            <label htmlFor="adults">Passengers 18+:</label>
+            <input id="adults" className="adult-input" type="number" placeholder="1" />
 
-                <label for="children">Passengers under 18:</label> 
-                <input id="children" className="child-input" type="number" placeholder="0" />
+            <label htmlFor="children">Passengers under 18:</label>
+            <input id="children" className="child-input" type="number" placeholder="0" />
+
+            <div className="input-number">
+                <input type="number" value={amount} readOnly />
+                <div className="input-number-actions">
+                    <button onClick={addValue}>+</button>
+                    <button onClick={subtractValue}>−</button>
+                </div>
             </div>
-        </>
+
+
+        </div>
     );
 };
 
