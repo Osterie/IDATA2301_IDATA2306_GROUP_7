0250e9bb13b7ff37cdfc8ede6c1a89274c715c1e
TobyJavascript <98916721+TobyJavascript@users.noreply.github.com>
2025-04-01 13:48:43 +0200

    Added logger to backend fav-classes

--- backend-Idata2306/code/stud/src/main/java/ntnu/no/stud/controllers/FavoriteFlightController.java
+++ backend-Idata2306/code/stud/src/main/java/ntnu/no/stud/controllers/FavoriteFlightController.java
@@ -1,10 +1,10 @@
 package ntnu.no.stud.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
-
 import ntnu.no.stud.services.FavoriteFlightService;
-
-import ntnu.no.stud.entities.*;
+import ntnu.no.stud.entities.FavoriteFlight;
 
 import java.util.List;
 
@@ -13,24 +13,37 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:3000") // Enable CORS for React
 public class FavoriteFlightController {
 
+    private static final Logger logger = LoggerFactory.getLogger(FavoriteFlightController.class);  // Logger instance
+
     private final FavoriteFlightService service;
 
     public FavoriteFlightController(FavoriteFlightService service) {
         this.service = service;
     }
 
+    // Get all favorite flights
     @GetMapping
     public List<FavoriteFlight> getAllFlights() {
-        return service.getAllFlights();
+        logger.info("Fetching all favorite flights");
+        List<FavoriteFlight> flights = service.getAllFlights();
+        logger.info("Found {} flights", flights.size());  // Log the number of flights retrieved
+        return flights;
     }
 
+    // Add a new favorite flight
     @PostMapping
     public FavoriteFlight addFlight(@RequestBody FavoriteFlight flight) {
-        return service.addFlight(flight);
+        logger.info("Adding new favorite flight: {}", flight);
+        FavoriteFlight addedFlight = service.addFlight(flight);
+        logger.info("Successfully added flight: {}", addedFlight);
+        return addedFlight;
     }
 
+    // Delete a favorite flight by ID
     @DeleteMapping("/{id}")
     public void deleteFlight(@PathVariable Long id) {
+        logger.info("Attempting to delete flight with ID: {}", id);
         service.deleteFlight(id);
+        logger.info("Successfully deleted flight with ID: {}", id);
     }
 }
--- backend-Idata2306/code/stud/src/main/java/ntnu/no/stud/services/FavoriteFlightService.java
+++ backend-Idata2306/code/stud/src/main/java/ntnu/no/stud/services/FavoriteFlightService.java
@@ -1,27 +1,42 @@
 package ntnu.no.stud.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import ntnu.no.stud.repositories.*;
-import ntnu.no.stud.entities.*;
+import ntnu.no.stud.repositories.FavoriteFlightRepository;
+import ntnu.no.stud.entities.FavoriteFlight;
+
 import java.util.List;
 
 @Service
 public class FavoriteFlightService {
-    
+
+    private static final Logger logger = LoggerFactory.getLogger(FavoriteFlightService.class);  // Logger instance
+
     private final FavoriteFlightRepository repository;
 
     public FavoriteFlightService(FavoriteFlightRepository repository) {
         this.repository = repository;
     }
 
+    // Get all flights
     public List<FavoriteFlight> getAllFlights() {
+        logger.debug("Fetching all flights from the repository");
         return repository.findAll();
     }
 
+    // Add a new flight
     public FavoriteFlight addFlight(FavoriteFlight flight) {
-        return repository.save(flight);
+        logger.debug("Saving new flight: {}", flight);
+        FavoriteFlight savedFlight = repository.save(flight);
+        logger.debug("Saved flight: {}", savedFlight);
+        return savedFlight;
     }
 
+    // Delete a flight
     public void deleteFlight(Long id) {
+        logger.debug("Deleting flight with ID: {}", id);
         repository.deleteById(id);
+        logger.debug("Deleted flight with ID: {}", id);
     }
 }
