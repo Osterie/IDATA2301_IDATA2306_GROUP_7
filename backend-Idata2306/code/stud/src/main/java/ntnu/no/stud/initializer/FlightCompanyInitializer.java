0dda23d3ba793efdb2c4b7f491684b136588276d
TobyJavascript <98916721+TobyJavascript@users.noreply.github.com>
2025-05-13 13:41:14 +0200

    Added image links

--- backend-Idata2306/code/stud/src/main/java/ntnu/no/stud/initializer/FlightCompanyInitializer.java
+++ backend-Idata2306/code/stud/src/main/java/ntnu/no/stud/initializer/FlightCompanyInitializer.java
@@ -22,17 +22,18 @@ public class FlightCompanyInitializer {
 
     @PostConstruct
     public void init() {
-        createCompany("American Airlines", "https://aa.com", "https://logo.com/aa.png");
-        createCompany("Delta Air Lines", "https://delta.com", "https://logo.com/delta.png");
-        createCompany("Lufthansa", "https://lufthansa.com", "https://logo.com/lh.png");
-        createCompany("Emirates", "https://emirates.com", "https://logo.com/ek.png");
-        createCompany("Qatar Airways", "https://qatarairways.com", "https://logo.com/qr.png");
-        createCompany("Singapore Airlines", "https://singaporeair.com", "https://logo.com/sq.png");
-        createCompany("Air France", "https://airfrance.com", "https://logo.com/af.png");
-        createCompany("KLM Royal Dutch Airlines", "https://klm.com", "https://logo.com/klm.png");
-        createCompany("Norwegian Air Shuttle", "https://norwegian.com", "https://logo.com/norwegian.png");
-        createCompany("Alitalia", "https://alitalia.com", "https://logo.com/alitalia.png");
-        createCompany("Swiss International Air Lines", "https://swiss.com", "https://logo.com/swiss.png");
+        createCompany("American Airlines", "https://aa.com", "https://images.app.goo.gl/n1EZ86hcbnWW3r399");
+        createCompany("Delta Air Lines", "https://delta.com", "https://images.app.goo.gl/zYxSrW6yM3jmM6cV7");
+        createCompany("Lufthansa", "https://lufthansa.com", "https://images.app.goo.gl/YCBLVgLaUvVErdV38");
+        createCompany("Emirates", "https://emirates.com", "https://images.app.goo.gl/Hk3AvtacEWiunjzaA");
+        createCompany("Qatar Airways", "https://qatarairways.com", "https://images.app.goo.gl/yWLgMi6xprkJmj6Q6");
+        createCompany("Singapore Airlines", "https://singaporeair.com", "https://images.app.goo.gl/75nuboiT4msQSiN38");
+        createCompany("Air France", "https://airfrance.com", "https://images.app.goo.gl/3wc7F7SRaH2TtiYSA");
+        createCompany("KLM Royal Dutch Airlines", "https://klm.com", "https://images.app.goo.gl/nbFzpG7DNH5AKmke9");
+        createCompany("Norwegian Air Shuttle", "https://norwegian.com", "https://images.app.goo.gl/kRcdsYSS7d6zqa916 ");
+        createCompany("Alitalia", "https://alitalia.com", "https://images.app.goo.gl/pYegULxoezf6uuzS6");
+        createCompany("Swiss International Air Lines", "https://swiss.com", "https://images.app.goo.gl/6Wb4Tnvkoz8mxDnM8");
+        createCompany("British Airways", "https://www.britishairways.com", "https://images.app.goo.gl/KiPmGtr5sJXshpmq9");
     }
 
     private void createCompany(String name, String website, String imageUrl) {
