86f296c5033661311e746b16800d9ae75aa1e684
Daniel <daniesel@stud.ntnu.no>
2025-05-15 13:44:00 +0200

    Updated purchaseRequest in backend to get correct information

--- frontend-Idata2301/code/app/src/utils/shoppingCartUtils.js
+++ frontend-Idata2301/code/app/src/utils/shoppingCartUtils.js
@@ -1,4 +1,5 @@
 import Cookies from "js-cookie";
+import { getCookie, setCookie } from "../library/tools";
 
 const COOKIE_NAME = "shoppingCart";
 
@@ -13,7 +14,11 @@ export const addToShoppingCart = (flight) => {
   const cart = getShoppingCart();
   if (!cart.some((item) => item.id === flight.id)) {
     const updatedCart = [...cart, flight];
-    Cookies.set(COOKIE_NAME, JSON.stringify(updatedCart), { expires: 7 }); // Save to cookies (expires in 7 days)
+    console.log(getCookie(COOKIE_NAME));
+    console.log(updatedCart);
+    setCookie(COOKIE_NAME, updatedCart, 1000*60*60*24*7); // Save to cookies (expires in 7 days)
+    //Cookies.set(COOKIE_NAME, JSON.stringify(updatedCart), { expires: 7 }); // Save to cookies (expires in 7 days)
+    console.log(getCookie(COOKIE_NAME));
     return { success: true, message: "Flight added to the shopping cart!" };
   }
   return { success: false, message: "This flight is already in your shopping cart!" };
