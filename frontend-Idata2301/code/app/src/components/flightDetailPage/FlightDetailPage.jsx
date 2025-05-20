1e55432c28228a1f8c1b185670214f469a177ce1
TobyJavascript <98916721+TobyJavascript@users.noreply.github.com>
2025-05-20 16:31:33 +0200

    Commit to get newest content

--- frontend-Idata2301/code/app/src/components/flightDetailPage/FlightDetailPage.jsx
+++ frontend-Idata2301/code/app/src/components/flightDetailPage/FlightDetailPage.jsx
@@ -5,7 +5,7 @@ import { getCompanyImage } from "../../library/imageRequests";
 import { sendApiRequest } from "../../library/requests";
 import flightHeroImage from "../../resources/images/avel-chuklanov-Ou1eqo29Ums-unsplash.jpg";
 
-const FlightDetailPage = ({ searchParams, flight, setActivePage}) => {
+const FlightDetailPage = ({ searchParams, flight, setActivePage, handleGoBack}) => {
   const [companyImageUrl, setCompanyImageUrl] = useState(null);
   const [providerAlternatives, setProviderAlternatives] = useState([]);
 
@@ -74,7 +74,9 @@ const FlightDetailPage = ({ searchParams, flight, setActivePage}) => {
   return (
     <section>
       <header className="flight-hero-image">
-        <button onClick={() => setActivePage("deals")} className="back-button">← Back to admin page</button>
+        <button onClick={handleGoBack} className="back-button">
+          ← Go Back
+        </button>
         <img
           src={companyImageUrl || flightHeroImage}
           alt="Company or default flight image"
