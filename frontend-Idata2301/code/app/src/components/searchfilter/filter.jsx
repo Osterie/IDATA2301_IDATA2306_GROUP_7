eca184b8740a575c02335d31c04041fb2729a6b4
OSTERITE <adrian@bandidos.org>
2025-05-21 10:58:21 +0200

    moved filter functions to filterUtils.js

--- frontend-Idata2301/code/app/src/components/searchfilter/filter.jsx
+++ frontend-Idata2301/code/app/src/components/searchfilter/filter.jsx
@@ -2,99 +2,13 @@ import { useState, useEffect, useRef } from "react";
 import "./filter.css";
 import DualRangeSlider from "./slider";
 import { calculateFinalPriceInUserCurrency } from "../../utils/currencyUtils";
-
-function findMinPrice(flights) {
-  return Math.min(
-    ...flights.map((flight) =>
-      calculateFinalPriceInUserCurrency(
-        flight.price,
-        flight.discount,
-        flight.currencyCode
-      )
-    )
-  );
-}
-function findMaxPrice(flights) {
-  return Math.max(
-    ...flights.map((flight) =>
-      calculateFinalPriceInUserCurrency(
-        flight.price,
-        flight.discount,
-        flight.currencyCode
-      )
-    )
-  );
-}
-
-// Dynamically extract unique companies from the flights array
-function getUniqueCompanies(flights) {
-  const companies = flights.map(
-    (flight) => flight.scheduledFlight.flight.company.name
-  );
-  return [...new Set(companies)]; // Remove duplicates
-}
-
-const filterFlights = (flightsToFilter, min, max, companies) => {
-  const updatedFlights = flightsToFilter.map((flight) => {
-    const flightCompany = flight.scheduledFlight.flight.company.name;
-    const flightPrice = calculateFinalPriceInUserCurrency(
-      flight.price,
-      flight.discount,
-      flight.currencyCode
-    );
-    const isPriceInRange = flightPrice >= min && flightPrice <= max;
-    const isCompanySelected = companies[flightCompany];
-
-    const isFilteredOut = !(isPriceInRange && isCompanySelected);
-    return { ...flight, isFilteredOut };
-  });
-  return updatedFlights;
-};
-
-const sortFlights = (flightsToSort, option) => {
-  const sorted = [...flightsToSort];
-
-  switch (option) {
-    case "cheap":
-      sorted.sort(
-        (a, b) =>
-          calculateFinalPriceInUserCurrency(
-            a.price,
-            a.discount,
-            a.currencyCode
-          ) -
-          calculateFinalPriceInUserCurrency(b.price, b.discount, b.currencyCode)
-      );
-      break;
-    case "expensive":
-      sorted.sort(
-        (a, b) =>
-          calculateFinalPriceInUserCurrency(
-            b.price,
-            b.discount,
-            b.currencyCode
-          ) -
-          calculateFinalPriceInUserCurrency(a.price, a.discount, a.currencyCode)
-      );
-      break;
-    case "soon":
-      sorted.sort(
-        (a, b) =>
-          new Date(a.scheduledFlight.date) - new Date(b.scheduledFlight.date)
-      );
-      break;
-    case "late":
-      sorted.sort(
-        (a, b) =>
-          new Date(b.scheduledFlight.date) - new Date(a.scheduledFlight.date)
-      );
-      break;
-    default:
-      break;
-  }
-
-  return sorted;
-};
+import {
+  findMinPrice,
+  findMaxPrice,
+  getUniqueCompanies,
+  sortFlights,
+  filterFlights,
+} from "../../utils/filterUtils";
 
 const FilterSidebar = ({ flights, setFlights }) => {
   const [sortOption, setSortOption] = useState("cheap");
@@ -192,11 +106,8 @@ const FilterSidebar = ({ flights, setFlights }) => {
 
   const handlePriceRangeChange = (min, max) => {
     setPriceRange({ min, max }); // Save the values for later use
-
     let updatedFlights = filterFlights(flights, min, max, selectedCompanies);
-
     updatedFlights = sortFlights(updatedFlights, sortOption);
-
     setFlights(updatedFlights);
   };
 
